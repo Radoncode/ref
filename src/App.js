3dcd8705336040255813383e9344cd86c7376c59
Radoncode <relbouni69@gmail.com>
2022-02-02 17:02:48 +0000

    Using forwardRef

--- src/App.js
+++ src/App.js
@@ -11,13 +11,14 @@ class App extends Component {
   }
 
   handleClick = () => {
-    this.refComp.current.addFocus();
+    this.refComp.current.focus();
   }
 
   render(){
+    console.log(this.refComp);
     return (
       <div className="App">
-        <MyRef ref={this.refComp}/>
+        <MyRef name="toto" ref={this.refComp}/>
         <button onClick={this.handleClick}>Valider</button>
       </div>
     );
--- src/MyRef.js
+++ src/MyRef.js
@@ -1,30 +1,15 @@
 import React, { Component } from "react";
 
-class MyRef extends Component {
+const MyRef = React.forwardRef((props, ref) => {
+    console.log(props);
+    console.log(ref);
+    return(
+        <div>
+            <input ref={ref}  type="text" />
+        </div>
+    )
+})
 
-    constructor(props) {
-        super(props);
 
-        this.state = {
-            value: ''
-        }
-
-        this.myTitle = React.createRef();
-        this.myInput = React.createRef();
-    }
-
-    
-    addFocus = () =>  {
-        this.myInput.current.focus();
-    }
-
-    render() {
-        return(
-            <div>
-                <input ref={this.myInput} type="text" />
-            </div>
-        )
-    }
-}
 
 export default MyRef;
